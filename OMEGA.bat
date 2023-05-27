baf8a991218f7344f0e2d3c64d34f8adc60ea2bb
Sebix12 <93684044+Sebix12@users.noreply.github.com>
2023-05-27 05:25:06 +0200

    Add files via upload

--- OMEGA.bat
+++ OMEGA.bat
@@ -7,7 +7,8 @@ call filesystem.bat checkup
 goto :start
 
 :installfs
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/omega/main/filesystem.bat', 'filesystem.bat')"
+::powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/omega/main/filesystem.bat', 'filesystem.bat')"
+echo $info-    Installed filesystem.bat
 goto :contfscheck
 
 ::folders check
@@ -24,7 +25,33 @@ goto :cbind
 
 :cdb
 call filesystem.bat mkdir db
+call filesystem.bat getbasetools
 goto :cdbd
 
 ::end of checkup
 :mainexec
+::import config if exist
+if exist config.bat call config.bat
+if not exist config.bat :atcconf
+
+:atcconf
+echo.
+echo Would you like to create a config? (y:n)
+set /p tempchoice=">"
+if "%tempchoice%" equ "y" goto :cconfig
+if "%tempchoice%" equ "n" goto :sconfig
+
+:sconfig
+echo Skipping!
+echo.
+goto :terminal
+
+:cconfig
+echo set defloc=%~dp0
+echo done!
+echo.
+goto :terminal
+
+:terminal
+pause
+exit
--- filesystem.bat
+++ filesystem.bat
@@ -2,6 +2,7 @@
 if "%1" equ "checkup" echo $info-    filesystem.bat is ok!
 if "%1" equ "mkdir" goto :mkdir
 if "%1" equ "rmdir" goto :rmdir
+if "%1" equ "getbasetools" goto :gettools
 goto :ext
 
 :mkdir
@@ -12,6 +13,15 @@ goto :ext
 :rmdir
 rmdir %2
 echo $info-    Deleted folder %2
+goto :ext
+
+:gettools
+cd db
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/omega/main/db.zip', 'tools.zip')"
+echo $info-    Downloaded Tar zip
+tar -xf tools.zip
+del tools.zip
+cd ..
 goto :ext
 
 :ext
