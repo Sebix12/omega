a5c241e1244a47569657e83569449b8dc963321b
Sebix12 <93684044+Sebix12@users.noreply.github.com>
2023-05-27 05:11:19 +0200

    Add files via upload

--- filesystem.bat
+++ filesystem.bat
@@ -1,5 +1,17 @@
 @echo off
-if "%1" equ "checkup" $info-    filesystem.bat is ok!
+if "%1" equ "checkup" echo $info-    filesystem.bat is ok!
+if "%1" equ "mkdir" goto :mkdir
+if "%1" equ "rmdir" goto :rmdir
+goto :ext
+
+:mkdir
+mkdir %2
+echo $info-    Created folder %2
+goto :ext
+
+:rmdir
+rmdir %2
+echo $info-    Deleted folder %2
 goto :ext
 
 :ext
